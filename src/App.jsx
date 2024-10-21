1470dfd456f8f0da4cb8dcd04e64c4d5a9d6a9e0
AntonWitjaksono1412 <antonwitjaksono1412@gmail.com>
2024-10-21 12:10:28 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from './components/Sidebar/index';
+
+
+function App() {
+  return (
+    <div className='w-full min-h-screen'>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
